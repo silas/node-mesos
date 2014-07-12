d9c0cfbaba02529bd294bf83daccc6dce0391aba
Silas Sewell <silas@sewell.org>
2014-07-11 23:56:48 -0700

    Improve job list test

--- test/chronos.js
+++ test/chronos.js
@@ -4,6 +4,8 @@
  * Module dependencies.
  */
 
+var async = require('async');
+var lodash = require('lodash');
 var should = require('should');
 var uuid = require('node-uuid');
 
@@ -23,11 +25,47 @@ describe('Chronos', function() {
     this.chronos.on('debug', helper.debug('mesos:chronos'));
   });
 
+  beforeEach(function(done) {
+    var self = this;
+
+    self.name = 'test-' + uuid.v4();
+    self.owner = 'owner@example.org';
+
+    var jobs = {};
+
+    jobs.create = function(cb) {
+      var options = {
+        schedule: 'R10/2012-10-01T05:52:00Z/PT2S',
+        name: self.name,
+        epsilon: 'PT15M',
+        command: 'true',
+        owner: self.owner,
+        async: false,
+      };
+
+      self.chronos.create(options, cb);
+    };
+
+    async.auto(jobs, done);
+  });
+
   it('should return jobs', function(done) {
+    var self = this;
+
     this.chronos.list(function(err, data) {
       should.not.exist(err);
 
       should(data).be.instanceof(Array);
+
+      var job = lodash.find(data, function(job) {
+        return self.name === job.name;
+      });
+
+      should.exist(job);
+
+      job.name.should.eql(self.name);
+      job.owner.should.eql(self.owner);
+      job.disabled.should.eql(false);
 
       done();
     });
